91d313c4f3908ac9a9ef1c407ae00e7515203650
santino majur <demalong22@gmail.com>
2024-02-08 23:57:00 +0200

    generates a random image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import memeData from "../memeData";
 
 export default function Meme() {
-    function handleClick(e) {
-        e.preventDefault()
-        console.log("I was clicked!")
-    }
-    function handleOnMouseHover() {
-        console.log("The mouse was moved over!")
-    }
+   const [memeImage, setMemeImage] = useState('')
+    function getMemeImage() {
+     const memesArray = memeData.data.memes
+     const randomNumber = Math.floor(Math.random() * memesArray.length)
+     const url = memesArray[randomNumber].url
+     setMemeImage(url)
+   }
+  
   return (
     <main className="p-8">
-      <form className="flex flex-col gap-4 mt-4">
+      <div className="flex flex-col gap-4 mt-4">
         <div className="flex justify-between">
-          <input className="border border-gray-600 py-1.5 px-4 rounded-md text-sm" type="text" placeholder="Top Text" />
-          <input className="border border-gray-600 py-1.5 px-4 rounded-md text-sm" type="text" placeholder="Bottom Text" />
+          <input
+            className="border border-gray-600 py-1.5 px-4 rounded-md text-sm"
+            type="text"
+            placeholder="Top Text"
+          />
+          <input
+            className="border border-gray-600 py-1.5 px-4 rounded-md text-sm"
+            type="text"
+            placeholder="Bottom Text"
+          />
         </div>
-        <div className="h-[150px] bg-cyan-800 hover:bg-sky-600" onMouseEnter={handleOnMouseHover}></div>
-        <button className="bg-gradient-to-r from-purple-700 to-purple-600 rounded-md py-1.5 px-5 text-white hover:bg-gradient-to-r hover:from-cyan-800 hover:to-cyan-600" onClick={handleClick}>Get a new meme image 🖼</button>
-      </form>
+         <button className="bg-gradient-to-r from-purple-700 to-purple-600 rounded-md py-1.5 px-5 text-white hover:bg-gradient-to-r hover:from-cyan-800 hover:to-cyan-600" onClick={getMemeImage}>Get a new meme image 🖼</button>  
+      </div>
+       <img src={memeImage} alt="meme" className="mt-6 max-w-full rounded-md"/> 
+     
     </main>
   );
 }
